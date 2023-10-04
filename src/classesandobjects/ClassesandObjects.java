7c46fde19be1a680ab0098b28b99fc17679c999a
2022525 <2022525@student.cct.ie>
2023-10-04 12:52:27 +0100

    update proper
    properly

--- src/classesandobjects/ClassesandObjects.java
+++ src/classesandobjects/ClassesandObjects.java
@@ -16,5 +16,27 @@ public class ClassesandObjects {
     public static void main(String[] args) {
         // TODO code application logic here
         Triangle tri1= new Triangle();
-    
+        Triangle tri2=new Triangle();
+        Triangle tri3=new Triangle();
+        Triangle tri4=new Triangle();
+        Triangle tri5=new Triangle();
+        Triangle tri6=new Triangle();
+        
+        int[] sideLengths= new int[18];
+        sideLengths[0]=5;
+        sideLengths[1]=8;
+        sideLengths[2]=9;
+        //tri5(4,7,2)
+           sideLengths[12]=4;
+           sideLengths[13]=7;
+           sideLengths[14]=2;
+           //we want to set the middle sid of tri to be 3
+           sideLengths[1]=3;
+           sideLengths[4]=3;
+           sideLengths[7]=3;
+           sideLengths[10]=3;
+           sideLengths[13]=3;
+           sideLengths[16]=3;
+        
+    }
 }
